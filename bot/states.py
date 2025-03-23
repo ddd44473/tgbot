9ca2b9ff3661ebb0865094ae1c8274910019750c
ddd44473 <lapinspb@gmail.com>
2025-03-23 11:26:50 +0300

    Delete bot directory

--- bot/states.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import ConversationHandler
-
-# Определяем состояния
-WAITING_FOR_IMAGE, WAITING_FOR_NAME, WAITING_FOR_RARITY, WAITING_FOR_STYLE, WAITING_FOR_PNAME = range(5)
-
-# Экспортируем состояния
-__all__ = ['WAITING_FOR_IMAGE', 'WAITING_FOR_NAME', 'WAITING_FOR_PNAME', 'WAITING_FOR_STYLE', 'WAITING_FOR_RARITY']
